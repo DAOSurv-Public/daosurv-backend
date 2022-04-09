177dc31ad2477a71c2b1c0386665491c95c6d54c
gitihm <32189851+gitihm@users.noreply.github.com>
2022-04-09 15:04:40 +0700

    remove code unused

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
 import { AnalysisService } from '../analysis/analysis.service';
-import { CovalenthqService } from '../covalenthq/covalenthq.service';
 
 @Injectable()
 export class TasksService {
